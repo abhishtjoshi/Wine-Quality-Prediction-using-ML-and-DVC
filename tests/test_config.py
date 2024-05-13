18bfad55af2ed659362f64ea632a8e16edcb94df
Abhisht Joshi <abhishtjoshi16@gmail.com>
2024-05-13 14:43:22 -0400

    web app structure added

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,5 +10,8 @@ def test_generic():
     with pytest.raises(NotInRange):
         if a not in range(10,20):
             raise NotInRange
-        else:
-            assert a==b
+
+def test_something():
+    a = 2
+    b = 2
+    assert True
